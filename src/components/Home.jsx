bbf39d5a611f1d20a59f3810c2d12dbf9d4c8faf
John Johnson <john.johnson986@gmail.com>
2025-07-21 14:31:49 -0700

    Updated links and adjusted project tabs

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
   return (
@@ -8,6 +9,14 @@ export default function Home() {
         Hi! I'm [Your Name], a passionate developer specializing in building
         modern web applications. Explore my work and get in touch!
       </p>
+      <a
+        href="https://drive.google.com/file/d/1AkwAia_6SGDStdsXgiPzLXEiqIZfS6Yb/view?usp=sharing"
+        className="resume-button"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View My Resume
+      </a>
     </section>
   );
 }
